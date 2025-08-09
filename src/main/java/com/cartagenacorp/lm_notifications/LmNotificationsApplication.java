5d478498cc034a34d3fd5cf437d5b95bb95d95a0
Diego-Pedrozo <diegopedrozovillarreal@gmail.com>
2025-08-09 09:26:46 -0500

    build: Generatin .war File instead of .jar

--- src/main/java/com/cartagenacorp/lm_notifications/LmNotificationsApplication.java
+++ src/main/java/com/cartagenacorp/lm_notifications/LmNotificationsApplication.java
@@ -2,12 +2,19 @@ package com.cartagenacorp.lm_notifications;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class LmNotificationsApplication {
+public class LmNotificationsApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(LmNotificationsApplication.class, args);
+	}
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(LmNotificationsApplication.class);
 	}
 
 }
